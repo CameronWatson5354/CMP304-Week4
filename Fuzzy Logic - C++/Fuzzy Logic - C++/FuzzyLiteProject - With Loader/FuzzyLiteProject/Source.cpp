ccc1623e9dcf523fe5dfec2d76c034d234ae338f
Cameron Watson <2201442@uad.ac.uk>
2024-10-10 11:54:09 +0100

    start fuzzy task

--- Fuzzy Logic - C++/Fuzzy Logic - C++/FuzzyLiteProject - With Loader/FuzzyLiteProject/Source.cpp
+++ Fuzzy Logic - C++/Fuzzy Logic - C++/FuzzyLiteProject - With Loader/FuzzyLiteProject/Source.cpp
@@ -14,8 +14,47 @@ int main()
 	* TODO - Define input, output and rules
 	*/
 
+	//input variable 1
+	InputVariable* obstacle = new InputVariable;
+	engine->addInputVariable(obstacle);
 
+	obstacle->setName("obstacle");
+	obstacle->setRange(0.000, 1.000);
+	obstacle->addTerm(new Triangle("left", 0, 0.05, 0.6));
+	obstacle->addTerm(new Triangle("right", 0.4, 0.95, 1.0));
 
+	//input variable 2
+	InputVariable* speed = new InputVariable;
+	engine->addInputVariable(speed);
+
+	speed->setName("speed");
+	speed->setRange(0.000, 1.000);
+	speed->addTerm(new Triangle("movingLeft", 0.0, 0.0, 0.6));
+	speed->addTerm(new Triangle("none", 0.3, 0.5, 0.7));
+	speed->addTerm(new Triangle("movingRight", 0.4, 1.0, 1.0));
+
+	//output variable 1
+	OutputVariable* mSteer = new OutputVariable;
+	engine->addOutputVariable(mSteer);
+	mSteer->setName("mSteer");
+	mSteer->setRange(0.000, 1.000);
+	mSteer->addTerm(new Triangle("left", 0.0, 0.3, 0.6));
+	mSteer->addTerm(new Triangle("right", 0.4, 0.7, 1.0));
+	mSteer->setDefuzzifier(new Centroid(100));
+
+	mSteer->setAggregation(new Maximum);
+	mSteer->setDefaultValue(fl::nan);
+
+	//rule block
+	RuleBlock* mamdani = new RuleBlock;
+	mamdani->setName("mamdani");
+	mamdani->setConjunction(fl::null);
+	mamdani->setDisjunction(fl::null);
+	mamdani->setImplication(new AlgebraicProduct);
+	mamdani->setActivation(new General);
+	mamdani->addRule(Rule::parse("if obstacle is left then mSteer is right", engine));
+	mamdani->addRule(Rule::parse("if obstacle is right then mSteer is left", engine));
+	engine->addRuleBlock(mamdani);
 
 
 	// Check the status and print error if engine not setup
@@ -27,6 +66,26 @@ int main()
 	/*
 	* TODO - Add a gameplay loop 
 	*/
+	while (true)
+	{
+		std::string input = "";
+		std::cin >> input;
+
+		std::stringstream ss(input);
+		float number = 0.f;
+		ss >> number;
+
+		std::cin >> input;
+		float number2 = 0.f;
+		ss << input;
+		ss >> number2;
+
+		obstacle->setValue(number);
+		engine->process();
+		std::cout << "obstacle.input = " << number << " => steer.output = " << mSteer->getValue() << std::endl;
+	}
+
+
 
 
 
